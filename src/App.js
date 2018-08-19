5a22db054944582aafe576a35815953d9b094e00
Daniel Levett <dlevett@hotmail.co.uk>
2018-08-19 23:09:44 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,264 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import classnames from 'classnames';
+import { css } from 'emotion';
+import { findClosestIndex } from 'find-closest';
+
+const POINT_RADIUS = 2;
+const POINT_RADIUS_ACTIVE = 6;
+const BLUR = 20;
+const RADIUS_WITH_BLUR = POINT_RADIUS_ACTIVE + (BLUR / 2);
+const POINT_HITBOX_SIZE = 30;
+
+// TODO: make a standard for referencing x, y coordinates. E.g., ALWAYS use an array, or ALWAYS an object. not both
+
+
+const adsrCanvas = css`
+  cursor: grab;
+`;
+const adsrCanvasGrabbing = css`
+  cursor: grabbing;
+`;
+
+const clearCanvas = (ctx) => {
+  const { canvas } = ctx;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+}
+const drawCircle = (ctx, x, y, radius) => {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+  ctx.fill();
+  ctx.stroke();
+}
+
+const getRelativeMouseCoordinates = (event) => {
+  const bounds = event.target.getBoundingClientRect();
+
+  return {
+    x: event.clientX - bounds.left,
+    y: event.clientY - bounds.top
+  };
+}
+
+class ADSR extends Component {
+  state = {
+    isMouseDown: false,
+    isMouseOver: false,
+    activeInput: null
+  };
+
+  getCurrentCoordinates = (props) => {
+    const { attack, decay, sustain, release } = props;
+    const { canvas } = this;
+    // TODO: rename
+    const offset = value => (value * 30) + RADIUS_WITH_BLUR;
+    const xTop = RADIUS_WITH_BLUR;
+    const xBottom = canvas.height - RADIUS_WITH_BLUR;
+    // TODO: Got lost in this one... make sure it makes sense:
+    const ySustain = canvas.height - ((sustain * (canvas.height - (2 * RADIUS_WITH_BLUR))) + RADIUS_WITH_BLUR)
+    const widthSustain = 50;
+
+    return [
+      [RADIUS_WITH_BLUR, xBottom],
+      [offset(attack), xTop],
+      [offset(attack + decay), ySustain],
+      [offset(attack + decay) + widthSustain, ySustain],
+      [offset(attack + decay + release) + widthSustain, xBottom],
+    ]
+  }
+
+  updateCanvas = (props, state) => {
+    const { canvas } = this;
+    const ctx = canvas.getContext('2d');
+    const focusedPointIndex = this.getFocusedElemIndex(state);
+    // TODO: move these out to  a styling fn
+
+    ctx.lineJoin  = 'bevel';
+    ctx.lineWidth = 2;
+    ctx.fillStyle = '#4286f4';
+    ctx.strokeStyle = '#4286f4';
+    ctx.shadowBlur = BLUR;
+    ctx.shadowColor = '#4286f4';
+
+
+    clearCanvas(ctx);
+
+    const points = this.getCurrentCoordinates(props);
+
+    ctx.beginPath();
+    points.forEach(([x, y], i) => {
+      if (i === 0) {
+        ctx.moveTo(x, y);
+      } else {
+        ctx.lineTo(x, y);
+      }
+    });
+    ctx.stroke();
+
+    // TODO: make this an external fn for on active/ hover
+    if (state.activeInput || state.isMouseOver) {
+      ctx.save();
+      ctx.globalAlpha = 0.08
+      ctx.fill()
+      ctx.restore();
+    }
+
+    points.forEach(([x, y], i) => {
+      const radius = i === focusedPointIndex
+        ? POINT_RADIUS_ACTIVE
+        : POINT_RADIUS;
+
+      drawCircle(ctx, x, y, radius);
+    });
+  }
+
+  getClosestPoint(x, y) {
+    const points = this.getCurrentCoordinates(this.props);
+    const distances = points.map(([x2, y2]) => Math.abs(x - x2) + Math.abs(y - y2));
+    const index = findClosestIndex(distances, 0);
+
+    return {
+      distance: distances[index],
+      point: points[index],
+      inputElement: this.pointIndexMap[index],
+      index
+    }
+  }
+
+  onCanvasMouseMove = (event) => {
+    const { x, y } = getRelativeMouseCoordinates(event);
+    const { inputElement, distance } = this.getClosestPoint(x, y);
+
+    if (inputElement && distance < POINT_HITBOX_SIZE) {
+      if (inputElement !== document.activeElement) {
+        inputElement.focus();
+      }
+    } else if (this.state.activeInput && !this.state.isMouseDown) {
+      this.state.activeInput.blur();
+    }
+  }
+
+  getFocusedElemIndex(state) {
+    // TODO: This fn is naff now it has an argument. May be wrong abstraction
+    state = state || this.state;
+    if (!state.activeInput) return null;
+
+    return this.pointIndexMap.indexOf(state.activeInput);
+  }
+
+  componentDidMount() {
+    this.pointIndexMap = [
+      undefined,
+      this.attack,
+      this.decay,
+      this.sustain,
+      this.release,
+    ]
+    this.updateCanvas(this.props, this.state);
+  }
+
+  componentWillUpdate(props, state) {
+    this.updateCanvas(props, state);
+  }
+
+  onMouseDown = (event) => {
+    this.setState({ isMouseDown: true });
+    event.preventDefault();
+    console.log('oh dear')
+  }
+
+  onMouseUp = () => {
+    this.setState({ isMouseDown: false });
+  }
+
+  onMouseEnter = () => {
+    this.setState({ isMouseOver: true })
+  }
+
+  onMouseLeave = () => {
+    this.setState({ isMouseOver: false })
+  }
+
+  onFocus = () => {
+    console.log('focusing!')
+    this.setState({ activeInput: document.activeElement })
+  }
+
+  onBlur = () => {
+    console.log('blurring!')
+
+    this.setState({ activeInput: null })
+  }
+
+  render() {
+    const {
+      attack,
+      decay,
+      sustain,
+      release,
+      onChange,
+    } = this.props;
+    const { isMouseDown } = this.state;
+
+    return (
+      <div
+        onChange={onChange}
+        onFocus={this.onFocus}
+        onBlur={this.onBlur}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+      >
+        <input type="range" step="0.05" min="0" max="2" name="attack" value={attack} ref={el => this.attack = el} />
+        <input type="range" step="0.05" min="0" max="2" name="decay" value={decay} ref={el => this.decay = el} />
+        <input type="range" step="0.05" min="0" max="1" name="sustain" value={sustain} ref={el => this.sustain = el} />
+        <input type="range" step="0.05" min="0" max="2" name="release" value={release} ref={el => this.release = el} />
+        <canvas
+          ref={el => this.canvas = el}
+          width="500"
+          height="400"
+          className={classnames(adsrCanvas, {[adsrCanvasGrabbing]: isMouseDown})}
+          onMouseMove={this.onCanvasMouseMove}
+          onMouseDown={this.onMouseDown}
+          onMouseUp={this.onMouseUp}
+        />
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+  state = {
+    adsrParams: {
+      attack: 1,
+      decay: 1,
+      sustain: 0.4,
+      release: 2,
+    },
+  }
+
+  onAdsrChange = ({ target }) => {
+    // TODO: Does this need some abstraction, or just plain event?
+    this.setState(({ adsrParams }) => ({
+      adsrParams: {
+        ...adsrParams,
+        [target.name]: Number(target.value),
+      }
+    }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Are you looking at my screen, Bay?</h1>
+        </header>
+        <ADSR {...this.state.adsrParams} onChange={this.onAdsrChange} />
+      </div>
+    );
+  }
+}
+
+export default App;
