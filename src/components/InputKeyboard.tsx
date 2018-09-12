f69e4a318b6f10b4d153e7dd0fe069fcdb35c6fc
Daniel Levett <dlevett@hotmail.co.uk>
2018-09-12 13:59:54 +0100

    Add black keys to keyboard

--- src/components/InputKeyboard.tsx
+++ src/components/InputKeyboard.tsx
@@ -7,17 +7,41 @@ import { css } from 'emotion';
 import MouseDownStatus from './util/MouseDownStatus';
 
 const keyContainer = css`
+	position: relative;
 	display: flex;
 	height: 200px;
 `;
 
-const keyWhite = css`
+const keyStyle = css`
 	appearance: none;
 	border: none;
+	cursor: pointer;
+`;
+
+const keyWhite = css`
+	${keyStyle}
 	outline: 1px solid #000;
 	flex: 1;
 	background: #fff;
-	cursor: pointer;
+
+	&:hover, &:focus {
+		background: #eee;
+	}
+
+	&:active {
+		background: #ccc;
+	}
+`;
+
+// TODO: Be consistent with ordering of words in "keyBlack" and "BlackKey"
+const keyBlack = css`
+	${keyStyle}
+	background: #000;
+	position: absolute;
+	top: 0;
+	bottom: 40%;
+	width: 40px;
+	transform: translateX(-50%);
 
 	&:hover, &:focus {
 		background: #eee;
@@ -40,7 +64,24 @@ class State {
 	numberOfWhiteKeys = 0;
 }
 
-const keyboardMap = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\\'];
+const KEYBOARD_MAP = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\\'];
+const WHITE_KEYS_WITH_BLACK = [0, 1, 3, 4, 5];
+const WHITE_KEYS_PER_OCTAVE = 8;
+
+// TODO: Move me
+interface KeyProps {
+	children?: React.ReactNode;
+	style?: React.CSSProperties;
+}
+const WhiteKey = ({ children, style }: KeyProps) =>
+	<button className={keyWhite} style={style}>{children}</button>;
+
+const BlackKey = ({ children, style }: KeyProps) =>
+	<button className={keyBlack} style={style}>{children}</button>;
+
+const hasBlackKey = (whiteKeyIndex: number) => {
+	return WHITE_KEYS_WITH_BLACK.includes(whiteKeyIndex % (WHITE_KEYS_PER_OCTAVE - 1));
+};
 
 class InputKeyboardBase extends Component<Props> {
 	public state = new State();
@@ -49,17 +90,25 @@ class InputKeyboardBase extends Component<Props> {
 	container: RefObject<HTMLDivElement> = createRef();
 
 	private readonly updateNumberOfWhiteKeys = () => {
-		if (!this.container.current) {
-			return;
-		}
-
 		this.setState({
-			numberOfWhiteKeys: Math.floor(this.container.current.clientWidth / this.props.keyWidth),
+			numberOfWhiteKeys: Math.floor(this.getWidth() / this.props.keyWidth),
 		});
 	};
 
+	private getWidth() {
+		if (!this.container.current) {
+			return 0;
+		}
+
+		return this.container.current.clientWidth;
+	}
+
+	private getWidthPerKey() {
+		return this.getWidth() / this.state.numberOfWhiteKeys;
+	}
+
 	private readonly onKeyDown = ({ key }: KeyboardEvent) => {
-		const index = keyboardMap.indexOf(key);
+		const index = KEYBOARD_MAP.indexOf(key);
 
 		if (index === -1) {
 			return;
@@ -87,11 +136,30 @@ class InputKeyboardBase extends Component<Props> {
 	}
 
 	public render() {
+		const widthPerKey = this.getWidthPerKey();
+		const { numberOfWhiteKeys } = this.state;
+
 		return (
 			<div className={keyContainer} ref={this.container}>
-				{range(this.state.numberOfWhiteKeys).map(i =>
-					<button key={i} className={keyWhite}>{keyboardMap[i] || ''}</button>,
-				)}
+				{range(numberOfWhiteKeys).map(i => {
+					const isLastKey = i === numberOfWhiteKeys - 1;
+					const nodes = [
+						<WhiteKey key='white'>
+							{KEYBOARD_MAP[i] || ''}
+						</WhiteKey>,
+					];
+
+					if (!isLastKey && hasBlackKey(i)) {
+						nodes.push(
+							<BlackKey
+								key='black'
+								style={{ left: widthPerKey * (i + 1) }}
+							/>,
+						);
+					}
+
+					return nodes;
+				})}
 			</div>
 		);
 	}
