8e263f47b227228bbfdfdf8629fc8873a7eb0d85
Daniel Levett <dlevett@hotmail.co.uk>
2018-12-09 21:25:15 +0000

    Add SVGLineCircle story

---
+++ src/stories/SVGLineCircle.stories.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { number } from '@storybook/addon-knobs';
+import SVGLineCircle from '../components/SVGLineCircle';
+import { css } from 'emotion';
+
+const styleOuterWrapper = css`
+	display: flex;
+`;
+
+const styleDemoWrapper = css`
+	padding: 30px;
+	margin: 15px;
+	border: 1px solid #444;
+`;
+
+const svgProps = {
+	viewBox: '0 0 100 100',
+	stroke: '#000',
+	fill: '#000',
+	height: '300',
+	width: '100%',
+	preserveAspectRatio: 'none',
+};
+
+storiesOf('SVGLineCircle', module)
+	.add('Basic usage', () => {
+		const radius = number('radius', 10, {
+			range: true,
+			min: 0,
+			max: 50,
+			step: 1,
+		});
+
+		return (
+			<div className={styleOuterWrapper}>
+				<div className={styleDemoWrapper}>
+					<h2>{'<SVGLineCircle>'}</h2>
+					<p>{'<SVGLineCircle> is always a perfect circle. The `strokeWidth` property can be used to set a predictable pixel value for the size.'}</p>
+					<p>Circle radius: <strong>{String(radius)}px</strong></p>
+					<svg {...svgProps}>
+						<SVGLineCircle
+							vectorEffect='non-scaling-stroke'
+							point={[50, 50]}
+							strokeWidth={radius * 2}
+						/>
+					</svg>
+				</div>
+				<div className={styleDemoWrapper}>
+					<h2>{'<circle>'}</h2>
+					<p>{'The aspect ratio of <circle> is not maintained when the SVG is stretched, and it is not possible to set the size of the shape independently from the size of the entire <svg> parent.'}</p>
+					<p>Circle radius: <strong>who knows mate...</strong></p>
+					<svg {...svgProps}>
+						<circle
+							vectorEffect='non-scaling-stroke'
+							cx='50'
+							cy='50'
+							r={radius}
+						/>
+					</svg>
+				</div>
+			</div>
+		);
+	});
--- src/stories/SVGPolyline.stories.tsx
+++ src/stories/SVGPolyline.stories.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { number } from '@storybook/addon-knobs';
-import SVGPathLine from '../components/SVGPolyline';
+import SVGPolyline from '../components/SVGPolyline';
 
 const rangeOptions = {
 	range: true,
 	min: 0,
-	max: 10,
-	step: 0.1,
+	max: 100,
+	step: 1,
 };
 
-storiesOf('SVGPathLine', module)
+storiesOf('SVGPolyline', module)
 	.add('Basic usage', () => (
 		<svg
-			viewBox='0 0 10 10'
+			viewBox='0 0 100 100'
 			stroke='#000'
 			strokeWidth='1'
 			fill='none'
@@ -21,16 +21,16 @@ storiesOf('SVGPathLine', module)
 			width='100%'
 			preserveAspectRatio='none'
 		>
-			<SVGPathLine
+			<SVGPolyline
 				vectorEffect='non-scaling-stroke'
 				pointsArray={[
-					[0, 5],
+					[0, 50],
 					[
-						number('x', 4, rangeOptions),
-						number('y', 10, rangeOptions),
+						number('x', 40, rangeOptions),
+						number('y', 100, rangeOptions),
 					],
-					[6, 2],
-					[10, 5],
+					[60, 20],
+					[100, 50],
 				]}
 			/>
 		</svg>
