efe27d979926a8e399625725964962ba5b272410
Daniel Levett <dlevett@hotmail.co.uk>
2018-12-15 19:40:01 +0000

    Add a "useMedia" hook

---
+++ src/hooks/useMedia.ts
@@ -0,0 +1,19 @@
+import { baseUseEventListener } from './useEventListener';
+import { useState, useLayoutEffect } from 'react';
+
+const useMedia = (query: string) => {
+	const [matches, setMatches] = useState(false);
+
+	useLayoutEffect(() => {
+		const media = window.matchMedia(query);
+		setMatches(media.matches);
+
+		return baseUseEventListener(media, 'change', (event: Event) => {
+			setMatches((event as MediaQueryListEvent).matches);
+		});
+	}, [query]);
+
+	return matches;
+};
+
+export default useMedia;
