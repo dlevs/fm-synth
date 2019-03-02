c5c7b8370a671ffbace6be66b6f19504e1f20fbc
Daniel Levett <dlevett@hotmail.co.uk>
2019-03-02 17:48:47 +0000

    Add vertical variant of InputRangeLine

--- src/components/InputRangeLine.tsx
+++ src/components/InputRangeLine.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
+import { cx, css } from 'emotion'
 import clamp from 'lodash/fp/clamp'
 import { MIDI_MIN, MIDI_MAX, scaleMIDIValueBetween } from '../lib/scales'
 import { getClosestPointIndex, expandPointConfigs, cumulativeX } from '../lib/pointUtils'
@@ -18,8 +19,12 @@ import InputRange, { Props as InputRangeProps } from './InputRange'
 // TODO: Share these props from InputEnvelope:
 
 interface Props extends InputRangeProps {
-
+	orient?: 'horizontal' | 'vertical'
 }
+
+const styleRotate = css({
+	transform: 'rotate(180deg)'
+})
 
 // TODO: Typing directly on the fn works in "withOwnState.tsx". Why not here?
 
@@ -27,6 +32,7 @@ export const InputRangeLine = (props: Props) => {
 	// Props
 	const {
 		value,
+		orient = 'horizontal',
 		// setValue,
 		// pointsConfig,
 		// divideWidth,
@@ -36,31 +42,36 @@ export const InputRangeLine = (props: Props) => {
 	const width = 100
 	const height = 6
 	const yCenter = height / 2
+	const isVertical = orient === 'vertical'
+	const flipPoint = ([x, y]: Point): Point =>
+		isVertical ? [y, x] : [x, y]
 
 	return <>
 		{/* <div className={styleVisuallyHidden}> */}
 			<InputRange {...props} max={max} />
 		{/* </div> */}
 		<svg
-			className={style.svg}
+			className={cx(style.svg, {
+				[styleRotate]: isVertical
+			})}
 			width='100%'
 			height='300'
-			viewBox={`0 0 ${width} ${height}`}
+			viewBox={`0 0 ${flipPoint([width, height]).join(' ')}`}
 			preserveAspectRatio='none'
 		>
 			<SVGPolyline
 				className={style.svgBase(color)}
 				pointsArray={[
-					[0, yCenter],
-					[width, yCenter]
+					flipPoint([0, yCenter]),
+					flipPoint([width, yCenter])
 				]}
 			/>
 			<SVGLineCircle
 				className={style.circle(color)}
-				point={[
+				point={flipPoint([
 					(Number(value) / Number(max)) * width,
 					yCenter
-				]}
+				])}
 			/>
 		</svg>
 	</>
--- src/stories/InputRangeLine.stories.tsx
+++ src/stories/InputRangeLine.stories.tsx
@@ -3,13 +3,17 @@ import { storiesOf } from '@storybook/react'
 import useValue from './storyHelpers/useValue'
 import InputRangeLine from '../components/InputRangeLine'
 
-const Demo = () => (
+const Demo = () => <>
 	<InputRangeLine
-		label='My Label'
-		name='myname'
+		label='My horizontal input'
 		{...useValue(10)}
 	/>
-)
+	<InputRangeLine
+		label='My vertical input'
+		{...useValue(10)}
+		orient='vertical'
+	/>
+</>
 
 storiesOf('InputRangeLine', module)
 	.add('Basic usage', () => (
