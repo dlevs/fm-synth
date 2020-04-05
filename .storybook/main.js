45ce686f591e3fef07b62d4b58a7eab1dc9dbdd8
Daniel Levett <dlevett@hotmail.co.uk>
2020-04-05 22:46:00 +0100

    Fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,65 @@
+const path = require('path')
+const webpack = require('webpack')
+const srcDir = path.resolve(__dirname, '../src')
+
 module.exports = {
+	stories: [
+		'../src/**/*.stories.(tsx|ts|jsx|js)'
+	],
 	addons: [
 		'@storybook/addon-knobs/register',
 		'@storybook/addon-actions/register',
 		'@storybook/addon-storysource/register'
 	],
-	webpackFinal: async config => {
+	webpackFinal: async (config, ...rest) => {
 		config.module.rules.push({
-			test: /\.(ts|tsx)$/,
+			test: /\.tsx?$/,
+			exclude: /node_modules/,
 			use: [
 				{
-					loader: require.resolve('ts-loader'),
+					loader: require.resolve('babel-loader')
 				},
-				// // Optional
-				// {
-				// 	loader: require.resolve('react-docgen-typescript-loader'),
-				// }
+				// require.resolve('react-docgen-typescript-loader')
 			]
-		})
-		config.resolve.extensions.push('.ts', '.tsx')
+		});
+
+		config.resolve.extensions = Array.from(
+			new Set([
+				...config.resolve.extensions,
+				'.ts', '.tsx', '.js', '.json'
+			])
+		)
+		config.plugins.push(
+			// Strip ".js" extensions from TypeScript file imports.
+			//
+			// Web browsers expect imports to end with ".js" - extensionless
+			// imports don't work in the browser. Due to this, all imports
+			// are written with ".js" extensions.
+			//
+			// But the Storybook build fails due to the mismatch between ".js"
+			// in the import, and the ".ts" file it is to be matched to.
+			//
+			// This hack feels unnecessary. Hopefully there's a better way...
+			new webpack.NormalModuleReplacementPlugin(/\.js$/, (resource) => {
+				if (!resource.contextInfo) return
+
+				const { issuer } = resource.contextInfo
+
+				if (
+					issuer &&
+					(issuer.endsWith('.tsx') || issuer.endsWith('.ts')) &&
+					resource.context.startsWith(srcDir)
+				) {
+					resource.request = resource.request.replace('.js', '')
+				}
+			}),
+			// Another hack to prevent resolving dependencies to snowpack's
+			// "web_modules" directory.
+			// TODO: See if this can be achieved via babelrc config.
+			new webpack.NormalModuleReplacementPlugin(/^\/web_modules\//, (resource) => {
+				resource.request = resource.request.replace('/web_modules/', '')
+			})
+		)
 
 		return config
 	}
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,13 +1,4 @@
-import requireContext from 'require-context.macro'
-import { configure, addDecorator } from '@storybook/react'
+import { addDecorator } from '@storybook/react'
 import { withKnobs } from '@storybook/addon-knobs'
 
-const req = requireContext('../src', true, /\.stories\.(js|ts)x?$/)
-
-function loadStories() {
-	req.keys().forEach(filename => req(filename))
-}
-
 addDecorator(withKnobs)
-// TODO: Can babel-loader be removed?
-configure(loadStories, module)
